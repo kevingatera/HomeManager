cc40f6ce33e1f7edbd6e7c2686dabc298570b8e6
Kevin GATERA <kgate066@uottawa.ca>
2017-10-24 14:18:27 -0400

    Added changes to mainActivity

--- TheBest.java
+++
@@ -1,7 +0,0 @@
-public class TheBest {
- public static void main(String[] args) {
-  System.out.println(" Bonjour, je m'appelle Kevin GATERA,\n Mon numero d'etudiant est 8725061,\n Je prends SEG2505 avec des amis.\n");
-  System.out.println(" Bonjour, je m'appelle Alex Nishimwe,\n Mon numero d'etudiant est 8314601,\n je participe dans le projet d'application mobile dans le cours SEG2505 \n");
-  
- }
-}
