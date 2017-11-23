1e49cc863ed0b38a0d290c4d2f89d2717293dd49
Kevin GATERA <kgate066@uottawa.ca>
2017-11-23 18:26:57 -0500

    Added various improvements to the Shopping and Tasks fragments

--- app/src/main/java/besmart/team/homemanager/fragment_main_shopping.java
+++ app/src/main/java/besmart/team/homemanager/fragment_main_shopping.java
@@ -1,10 +1,21 @@
 package besmart.team.homemanager;
 
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 /**
  * Created by kevingatera on 22/11/17.
@@ -12,10 +23,56 @@ import android.view.ViewGroup;
 
 public class fragment_main_shopping extends Fragment {
 
+    /* adding the firebase */
+    ArrayList<String> myArrayList = new ArrayList<>();
+    ListView myListView;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference();
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View rootView1 = inflater.inflate(R.layout.fragment_main_shopping, container, false);
-        return rootView1;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView2 = inflater.inflate(R.layout.fragment_main_shopping, container, false);
+
+        return rootView2;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        final ArrayAdapter<String> myArrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, myArrayList);
+
+        myListView = view.findViewById(R.id.list);
+        myListView.setAdapter(myArrayAdapter);
+
+        myRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                String myChildValues = dataSnapshot.getValue(String.class);
+                myArrayList.add(myChildValues);
+                myArrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                myArrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+                myArrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
--- app/src/main/java/besmart/team/homemanager/fragment_main_tasks.java
+++ app/src/main/java/besmart/team/homemanager/fragment_main_tasks.java
@@ -1,5 +1,8 @@
 package besmart.team.homemanager;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -7,7 +10,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import com.baoyz.swipemenulistview.SwipeMenu;
+import com.baoyz.swipemenulistview.SwipeMenuCreator;
+import com.baoyz.swipemenulistview.SwipeMenuItem;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -27,6 +37,7 @@ public class fragment_main_tasks extends Fragment {
 
     ArrayList<String> myArrayList = new ArrayList<>();
     ListView myListView;
+    // SwipeMenuListView myListView;
 
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getReference();
@@ -39,14 +50,61 @@ public class fragment_main_tasks extends Fragment {
         return rootView2;
     }
 
+
+
+    // retrieve data and populate upon startup of the app
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         final ArrayAdapter<String> myArrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1 , myArrayList);
 
-        myListView = view.findViewById(R.id.list);
+        // myListView = (SwipeMenuListView) view.findViewById(R.id.list);
+        myListView = (ListView) view.findViewById(R.id.list);
         myListView.setAdapter(myArrayAdapter);
+
+        /*
+
+        SwipeMenuCreator creator = new SwipeMenuCreator() {
+
+            @Override
+            public void create(SwipeMenu menu) {
+                // create "open" item
+                SwipeMenuItem openItem = new SwipeMenuItem(
+                        getActivity().getApplicationContext());
+                // set item background
+                openItem.setBackground(new ColorDrawable(Color.rgb(0xC9, 0xC9,
+                        0xCE)));
+                // set item width
+                openItem.setWidth(170);
+                // set item title
+                openItem.setTitle("Open");
+                // set item title fontsize
+                openItem.setTitleSize(18);
+                // set item title font color
+                openItem.setTitleColor(Color.WHITE);
+                // add to menu
+                menu.addMenuItem(openItem);
+
+                // create "delete" item
+                SwipeMenuItem deleteItem = new SwipeMenuItem(
+                        getActivity().getApplicationContext());
+                // set item background
+                deleteItem.setBackground(new ColorDrawable(Color.rgb(0xF9,
+                        0x3F, 0x25)));
+                // set item width
+                deleteItem.setWidth(170);
+                // set a icon
+                deleteItem.setIcon(R.drawable.ic_delete);
+                // add to menu
+                menu.addMenuItem(deleteItem);
+            }
+        };
+
+        // add the swipingAdapter to the listView
+        myListView.setMenuCreator(creator);
+
+        */
 
         myRef.addChildEventListener(new ChildEventListener() {
             @Override
@@ -64,7 +122,6 @@ public class fragment_main_tasks extends Fragment {
             @Override
             public void onChildRemoved(DataSnapshot dataSnapshot) {
                 myArrayAdapter.notifyDataSetChanged();
-
             }
 
             @Override
@@ -83,4 +140,49 @@ public class fragment_main_tasks extends Fragment {
         // myRef.setValue("Hello, World!");
 
     }
+
+    /*
+
+    public class TaskArrayAdapter extends ArrayAdapter<String>  {
+        private final Context context;
+        private final String[] values;
+
+        public TaskArrayAdapter(Context context, String[] values) {
+            super(context, R.layout.list_team_layout, values);
+            this.context = context;
+            this.values = values;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View rowView = inflater.inflate(R.layout.list_team_layout, parent, false);
+            TextView textView = (TextView) rowView.findViewById(R.id.line01);
+            ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
+            textView.setText(values[position]);
+            // Change the icon for Windows and iPhone
+            String s = values[position];
+            if (s == null || s.isEmpty() || s.equals("empty")) {
+                imageView.setImageResource(R.drawable.ic_logo_empty);
+            } else {
+                imageView.setImageResource(R.drawable.ic_logo_mil);
+            }
+            return rowView;
+        }
+    }
+    RÉSULTAT
+            resultOfPreviousSlide
+    GESTION DE MÉMOIRE
+    Un patron de conceptions utiles dans la gestion d'application et de mémoire est le Singleton.
+    SINGLETON
+    Patron de conception utile pour stocker les informations.
+    Une limite d'un seul instance assure la cohérence entre vos attentes et la réalité entre lecture / écriture.
+    singleton
+
+    */
+
+
 }
+
+
+
