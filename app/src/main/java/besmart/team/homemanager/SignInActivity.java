5021283c31503da216a1100538bfda085b6a1e3f
Kevin GATERA <kgate066@uottawa.ca>
2017-11-29 12:03:34 -0500

    Renamed the SignInActivity

--- app/src/main/java/besmart/team/homemanager/SignInActivity.java
+++ app/src/main/java/besmart/team/homemanager/SignInActivity.java
@@ -18,7 +18,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class SigninActivity extends AppCompatActivity implements View.OnClickListener {
+public class SignInActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button signInButton;
     private EditText loginEmail;
