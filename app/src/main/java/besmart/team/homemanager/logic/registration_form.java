fd9b33121a2c89b45449b2cc1bdbf4af41eb6c77
anish006 <anish006@uottawa.ca>
2017-11-30 21:35:22 -0500

    i just created registration_form layout to be displayed on UI

---
+++ app/src/main/java/besmart/team/homemanager/logic/registration_form.java
@@ -0,0 +1,15 @@
+package besmart.team.homemanager.logic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import besmart.team.homemanager.R;
+
+public class registration_form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_form);
+    }
+}
